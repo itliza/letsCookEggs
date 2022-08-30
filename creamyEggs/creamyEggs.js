008fd7180cc3d542f58b43b79955a13266caa70b
Liza Romaniuk <elizaveta.romaniuk@gmail.com>
2022-08-30 14:27:54 +0300

    project start

---
+++ creamyEggs/creamyEggs.js
@@ -0,0 +1,33 @@
+const time = 6;
+let amountTime = time * 60;
+
+const btnStart = document.querySelector('.btn_start');
+
+btnStart.addEventListener('click', ()=>{
+    timer = setInterval(calculateTime, 1000);
+
+})
+
+function calculateTime(){
+    const countDown = document.querySelector('.countdown');
+    countDown.style.display = 'block';
+    let minutes = Math.floor(amountTime/60);
+    let seconds = amountTime%60;
+
+    if(seconds < 10) {
+        seconds = "0" + seconds;
+    }
+    countDown.textContent = `${minutes} : ${seconds}`;
+    amountTime --;
+
+    if(amountTime < 0) {
+        stopTimer();
+        amountTime = 0;
+    }
+
+    function stopTimer(){
+        clearInterval(timer);
+    
+    }
+}
+
